e86aee0b5febffe7f139c44a0aa7aa52ff550928
Yana94Ko <91898700+Yana94Ko@users.noreply.github.com>
2024-02-26 09:50:23 +0900

    [init] init express server

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,16 @@
-console.log("test");
+import bodyParser from "body-parser";
+import Express from "express";
+
+const app = Express();
+const port = 5050;
+const jsonParser = bodyParser.json();
+
+app.use(jsonParser);
+
+app.get("/health-check", (_, res) => {
+  res.send("OK");
+});
+
+app.listen(port, () =>
+  console.log(`XX Backend server is running at port No.${port}`)
+);
